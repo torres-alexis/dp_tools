0d292dc1f8c3bfc8541c602edaee0aeedab3aea7
Alexis Torres <lextorres46@gmail.com>
2025-05-29 18:13:12 -0700

    bulkRNAseq schema: make Source Name and Has Tech Reps cols optional

--- dp_tools/config/schemas.py
+++ dp_tools/config/schemas.py
@@ -30,8 +30,8 @@ runsheet = {
             "paired_end": pa.Column(bool, check_single_value),
             "read1_path": pa.Column(str),
             "read2_path": pa.Column(str, required=False), # Expect if paired_end is True
-            "Source Name": pa.Column(str),
-            "Has Tech Reps": pa.Column(bool),
+            "Source Name": pa.Column(str, required=False),
+            "Has Tech Reps": pa.Column(bool, required=False),
         },
         # define checks at the DataFrameSchema-level
         checks=check_read2_path_populated_if_paired_end
